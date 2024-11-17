2752b1a229ef03ae9dd135aa7ef808a84ae06218
BeshrHmaid <BeshrHmaid@gmail.com>
2024-11-17 22:07:41 +0200

    fixed mobile auth code bug

--- lib/features/auth_code/presentation/pages/list_auth_codes.dart
+++ lib/features/auth_code/presentation/pages/list_auth_codes.dart
@@ -21,7 +21,7 @@ class AuthorizationCodePage extends StatelessWidget {
           ..fetchAllAuthorizationCodes(), // Initial fetch
         child: CustomScaffold(
             body: AdaptiveLayout(
-          mobileLayout: (context) => _AuthorizationCodeContent(),
+          mobileLayout: (context) => _MobileAuthorizationCodeContent(),
           tabletLayout: (context) => const SizedBox(),
           desktopLayout: (context) => _AuthorizationCodeContent(),
         )));
@@ -147,6 +147,126 @@ class _AuthorizationCodeContent extends StatelessWidget {
           ),
         ),
         const Expanded(flex: 3, child: SizedBox()),
+      ],
+    );
+  }
+}
+
+class _MobileAuthorizationCodeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController licenseeIdController = TextEditingController();
+
+    return Row(
+      children: [
+        const Expanded(child: SizedBox()),
+        Expanded(
+          flex: 16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomBreadcrumb(
+                items: const ['Home', 'Authorization codes'],
+                onTap: (index) {
+                  // Add navigation logic based on index
+                  if (index == 0) {
+                    GoRouter.of(context).go(AppRouter.kHomeView);
+                  } else if (index == 1) {
+                    // Navigate to Active Users
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text('Authorization Codes',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: licenseeIdController,
+                decoration: const InputDecoration(
+                  labelText: 'Search by Licensee ID',
+                  border: OutlineInputBorder(),
+                ),
+                onSubmitted: (value) {
+                  final id = int.tryParse(value);
+                  if (id != null) {
+                    // Trigger the search
+                    Provider.of<AuthorizationCodeViewModel>(context,
+                            listen: false)
+                        .searchByLicenseeId(id);
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    licenseeIdController.clear();
+                    Provider.of<AuthorizationCodeViewModel>(context,
+                            listen: false)
+                        .resetSearch(); // Reset search and show all codes
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: const Color(
+                        0xFF017278), // Set text color to white for contrast
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10), // Rounded corners
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 20), // Add padding
+                    elevation: 5, // Add a slight shadow
+                  ),
+                  child: const Text(
+                    'Show All Codes',
+                    style: TextStyle(
+                      fontSize: 16, // Increase font size for better readability
+                      fontWeight: FontWeight.bold, // Make the text bold
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Consumer<AuthorizationCodeViewModel>(
+                  builder: (context, viewModel, child) {
+                    debugPrint(
+                        'Rebuilding UI, loading: ${viewModel.isLoading}, count: ${viewModel.authorizationCodes.length}');
+                    // Show loading indicator while data is being fetched
+                    if (viewModel.isLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    // Show message if no authorization codes are found
+                    if (viewModel.authorizationCodes.isEmpty) {
+                      return const Center(
+                          child: Text('No authorization codes found'));
+                    }
+
+                    // Build the list of authorization codes
+                    return ListView.builder(
+                      itemCount: viewModel.authorizationCodes.length,
+                      itemBuilder: (context, index) {
+                        final code = viewModel.authorizationCodes[index];
+                        return ListTile(
+                          title: Text('Code: ${code.code}'),
+                          subtitle: Text('Licensee ID: ${code.licenseeId}'),
+                          trailing: Text('Expires: ${code.periodEndDate}'),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Expanded(child: SizedBox()),
       ],
     );
   }
