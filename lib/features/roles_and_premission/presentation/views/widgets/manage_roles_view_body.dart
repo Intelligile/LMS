726b807e778ef5008591e8bbf669fded19ed5f94
BeshrHmaid <boshr@canadasmm.com>
2024-12-28 22:51:03 +0200

    indented breadcrumb

--- lib/features/roles_and_premission/presentation/views/widgets/manage_roles_view_body.dart
+++ lib/features/roles_and_premission/presentation/views/widgets/manage_roles_view_body.dart
@@ -19,111 +19,112 @@ class ManageRolesViewBody extends StatelessWidget {
     // Trigger the authorities fetch when the widget is built
     context.read<AuthorityCubit>().getAuthorities();
 
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: BlocConsumer<AuthorityCubit, AuthorityState>(
-        listener: (context, state) {
-          if (state is AuthorityStateFailure) {
-            showSnackBar(context, state.errorMessage, Colors.red);
-          } else if (state is GetAuthorityStateSuccess) {
-            authorities = state.authorities;
-          }
-        },
-        builder: (context, state) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomBreadcrumb(
+    return BlocConsumer<AuthorityCubit, AuthorityState>(
+      listener: (context, state) {
+        if (state is AuthorityStateFailure) {
+          showSnackBar(context, state.errorMessage, Colors.red);
+        } else if (state is GetAuthorityStateSuccess) {
+          authorities = state.authorities;
+        }
+      },
+      builder: (context, state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: CustomBreadcrumb(
                 items: const ['Home', 'Roles & Permissions', 'manage roles'],
                 onTap: (index) {
                   if (index == 0) {
                     GoRouter.of(context).go(AppRouter.kHomeView);
-                  } else if (index == 1) {}
+                  } else if (index == 1) {
+                    GoRouter.of(context).go(AppRouter.kRolesAndPermissionView);
+                  }
                 },
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12),
-                child: Text(
-                  'Create, view and edit roles & permissions for this site. ',
-                  style: Styles.textStyle16,
-                  overflow: TextOverflow.visible,
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12),
+              child: Text(
+                'Create, view and edit roles & permissions for this site. ',
+                style: Styles.textStyle16,
+                overflow: TextOverflow.visible,
               ),
-              MediaQuery.sizeOf(context).width < 600
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          AppBarActionButton(
-                            ontap: () {
-                              GoRouter.of(context)
-                                  .push(AppRouter.kUpdateRoleView);
-                            },
-                            icon: Icons.update,
-                          ),
-                          AppBarActionButton(
-                            ontap: () {
-                              GoRouter.of(context)
-                                  .push(AppRouter.kAddNewRoleView);
-                            },
-                            icon: FontAwesomeIcons.plus,
-                            txt: 'Create new role',
-                            tColor: kPrimaryColor,
-                            bColor: Colors.white,
-                          )
-                        ],
-                      ),
-                    )
-                  : const SizedBox(),
-              const Text(
-                'Custom roles',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              state is AuthorityStateLoading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : Expanded(
-                      child: ListView.builder(
-                        itemCount: authorities.length,
-                        itemBuilder: (context, index) {
-                          final role = authorities[index];
-                          return Card(
-                            margin: const EdgeInsets.symmetric(vertical: 4),
-                            child: ListTile(
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(role.authority ?? ''),
-                                  InkWell(
-                                    onTap: () {
-                                      GoRouter.of(context).push(
-                                          AppRouter.kUsersView,
-                                          extra: role);
-                                    },
-                                    child: Text(
-                                      'view users',
-                                      style: Styles.textStyle20
-                                          .copyWith(color: kPrimaryColor),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              onTap: () {
-                                // Handle role tap
-                              },
-                            ),
-                          );
-                        },
-                      ),
+            ),
+            MediaQuery.sizeOf(context).width < 600
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        AppBarActionButton(
+                          ontap: () {
+                            GoRouter.of(context)
+                                .push(AppRouter.kUpdateRoleView);
+                          },
+                          icon: Icons.update,
+                        ),
+                        AppBarActionButton(
+                          ontap: () {
+                            GoRouter.of(context)
+                                .push(AppRouter.kAddNewRoleView);
+                          },
+                          icon: FontAwesomeIcons.plus,
+                          txt: 'Create new role',
+                          tColor: kPrimaryColor,
+                          bColor: Colors.white,
+                        )
+                      ],
                     ),
-            ],
-          );
-        },
-      ),
+                  )
+                : const SizedBox(),
+            const Text(
+              'Custom roles',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            state is AuthorityStateLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: authorities.length,
+                      itemBuilder: (context, index) {
+                        final role = authorities[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(vertical: 4),
+                          child: ListTile(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(role.authority ?? ''),
+                                InkWell(
+                                  onTap: () {
+                                    GoRouter.of(context).push(
+                                        AppRouter.kUsersView,
+                                        extra: role);
+                                  },
+                                  child: Text(
+                                    'view users',
+                                    style: Styles.textStyle20
+                                        .copyWith(color: kPrimaryColor),
+                                  ),
+                                )
+                              ],
+                            ),
+                            onTap: () {
+                              // Handle role tap
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        );
+      },
     );
   }
 }
